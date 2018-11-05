a65b281f8dbfa7e37af6ac0bb538a9b4bcc04196
Soreine <soreine.plume@gmail.com>
2018-11-05 15:39:10 +0100

    Actual flow error

--- test.js
+++ test.js
@@ -3,4 +3,4 @@ import { List } from 'immutable';
 import { someList } from 'some-module';
 
 // There should be an error here, but someList is typed as `any`
-const wrong: List<number> = someList;
+const wrong: string = someList;
